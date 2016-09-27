b8bd379e287d2e5a5de70d97280daf2b66f8d0ea
Luis Castillo <lcastillo@mc69540castillo.local>
2016-09-27 07:37:38 -0700

    adding working itoast and sample project

---
+++ iToastSample/iToastSample/ViewController.m
@@ -0,0 +1,80 @@
+//
+//  ViewController.m
+//  iToastSample
+//
+//  Created by Luis Castillo on 9/27/16.
+//  Copyright © 2016 LC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize locationSegmentedControl, showToastButton, messageToDisplayLabel, durationSegmentedControl;
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)showToast:(id)sender
+{
+    
+    //what message to display?
+    NSString * msgToDisplay = self.messageToDisplayLabel.text;
+    if ([msgToDisplay length ] < 1) {
+        msgToDisplay = @"Hello from iToast";
+    }
+    
+    //where do we display the toast message?
+    iToastGravity locationType;
+    switch (locationSegmentedControl.selectedSegmentIndex) {
+        //top
+        case 0:
+            locationType = iToastGravityTop;
+            break;
+        //center
+        case 1:
+            locationType = iToastGravityCenter;
+            break;
+        //bottom
+        case 2:
+            locationType = iToastGravityBottom;
+            break;
+        default:
+            
+            break;
+    }
+    
+    //how long to display message?
+    //  NOTE: this value can also be a integer (milliseconds displaying it)
+    iToastDuration duration ;
+    switch (durationSegmentedControl.selectedSegmentIndex)
+    {
+        //short
+        case 0:
+            duration = iToastDurationShort;
+            break;
+        //normal
+        case 1:
+            duration = iToastDurationNormal;
+            break;
+        //long
+        case 2:
+            duration = iToastDurationLong;
+            break;
+        default:
+            break;
+    }
+    
+    [[[[iToast makeText:msgToDisplay] setGravity:locationType] setDuration:duration] show];
+}//eo-a
+
+
+@end
